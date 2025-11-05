4b895b8153b675420120ed6815fce0034f5d29eb
ArdaAyas <arda.ayas@ozu.edu.tr>
2025-11-05 15:47:48 +0300

    Add files via upload

---
+++ CS393PROJECT/CS393PROJECT/src/main/java/com/carrental/CS393PROJECT/Cs393ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.carrental.CS393PROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs393ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs393ProjectApplication.class, args);
+	}
+
+}
