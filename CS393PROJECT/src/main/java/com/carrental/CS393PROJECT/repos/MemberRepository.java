80ea9ebafaf969e3493d01ae37ab491efecdb33c
ArdaAyas <arda.ayas@ozu.edu.tr>
2025-11-27 22:32:26 +0300

    Update MemberRepository.java

--- CS393PROJECT/src/main/java/com/carrental/CS393PROJECT/repos/MemberRepository.java
+++ CS393PROJECT/src/main/java/com/carrental/CS393PROJECT/repos/MemberRepository.java
@@ -1,14 +1,15 @@
 package com.carrental.CS393PROJECT.repos;
 
-
+import java.lang.reflect.Member;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.carrental.CS393PROJECT.model.Member;
-
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>{
-	Optional<Member> findByDrivingLicenseNumber(String drivingLicenseNumber);
+
+	Optional<Member> findById(Long id);
+	
+	
 }
