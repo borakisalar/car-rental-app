807e73bc3695b17393e368f999581f6203a070cf
Bora Kısalar <borakisalar@gmail.com>
2025-11-27 22:56:51 +0300

    minor changes

--- CS393PROJECT/src/main/java/com/carrental/CS393PROJECT/repos/LocationRepository.java
+++ CS393PROJECT/src/main/java/com/carrental/CS393PROJECT/repos/LocationRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocationService extends JpaRepository<Location, Long>{
+public interface LocationRepository extends JpaRepository<Location, Long>{
 	Optional<Location> findByCode(String code);
 }
